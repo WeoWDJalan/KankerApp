a4db30f0c2d7a0d0665292f8bba73952f4343a70
Nabhan <nabhanrifai@gmail.com>
2024-05-01 07:06:37 +0700

    update

---
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.dicoding.asclepius.view
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityMainBinding
+import com.yalantis.ucrop.UCrop
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentImageUri: Uri? = null
+
+    private val launcherGallery =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+            if (it != null) {
+//                moveToCrop(it)
+                currentImageUri = it
+                showImage()
+            } else {
+                showToast(getString(R.string.failed_pick_image))
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        updateButtonStatus()
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.analyzeButton.setOnClickListener { analyzeImage() }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            binding.previewImageView.setImageURI(it)
+        }
+        updateButtonStatus()
+    }
+
+    private fun analyzeImage() {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri.toString())
+        startActivity(intent)
+    }
+
+//    private fun moveToCrop(uri: Uri) {
+//        UCrop.of(uri, Uri.fromFile(cacheDir.resolve("${System.currentTimeMillis()}.jpg")))
+//            .start(this)
+//    }
+//
+//    @Deprecated("Deprecated in Java")
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        Log.d(TAG, "onActivityResult")
+//        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+//            currentImageUri = UCrop.getOutput(data!!)
+//            showImage()
+//        } else if (resultCode == UCrop.RESULT_ERROR) {
+//            val errorMessage = UCrop.getError(data!!)?.message.toString()
+//            showToast(errorMessage)
+//            Log.e(TAG, errorMessage)
+//        }
+//    }
+
+    private fun updateButtonStatus() {
+        binding.analyzeButton.isEnabled = currentImageUri != null
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
